b90a12edb70f3d2a9da94ccd2af0e910b9888e67
Michael Gilos <gilosmichael@gmail.com>
2021-10-16 08:53:07 +0800

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'standard/computed-property-even-spacing': 0,
+    'no-unused-expressions': 'off'
+  }
+}
