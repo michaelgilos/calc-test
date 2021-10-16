b24e66e9c853ef30e3b3ef853fafecf8337b95a3
Michael Gilos <gilosmichael@gmail.com>
2021-10-16 08:38:14 +0800

    initial commit

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
+
+const Calculator = () => {
+  return (
+  )
+};
